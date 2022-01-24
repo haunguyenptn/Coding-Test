8312afed84e83f2605962bfb951c4005c831e268
Hau Nguyen <hau.nguyen@ptnglobalcorp.com>
2022-01-24 18:06:30 +0700

    [develop] - Summary @Description - Add TaxCalculator test case

--- spec/lib/tax_calculator_spec.rb
+++ spec/lib/tax_calculator_spec.rb
@@ -3,6 +3,21 @@ require 'tax_calculator'
 
 describe TaxCalculator do
   describe '#receipt' do
-    # TODO: Spec this
+    let(:product) { Product.new('hperfume', 10) }
+    let(:order_item) { OrderItem.new(1, product) }
+    let(:order) { Order.new([order_item]) }
+    let(:receipt_item) { ReceiptItem.new(order_item, 1) }
+    let(:receipt) { Receipt.new([receipt_item]) }
+    let(:calc) do
+      TaxCalculator.new(
+        order,
+        [
+          SalesTax::BasicSalesTax.new(BigDecimal('0.10')),
+          SalesTax::ImportDuty.new(BigDecimal('0.05'))
+        ]
+      )
+    end
+
+    it { expect(calc.receipt).to eq(receipt) }
   end
 end
